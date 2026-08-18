97125575ee82cf50452f91af8e84444d65fa8e65
Manish <78523634+Manish0213@users.noreply.github.com>
2026-08-19 01:30:33 +0530

    Revert "Paypal Create Order"

--- src/main/java/com/manish/payments/service/interfaces/PaymentService.java
+++
@@ -1,9 +0,0 @@
-package com.manish.payments.service.interfaces;
-
-import com.manish.payments.pojo.CreateOrderRequest;
-import com.manish.payments.pojo.OrderResponse;
-
-public interface PaymentService {
-	
-	public OrderResponse createOrder(CreateOrderRequest createOrderRequest);
-}
